94064130e5bf04d0cedf9ce5c29dfc89f82d9513
genarogg <gena_gonzalez15@hotmail.com>
2020-01-09 18:10:10 -0400

    Mostrando Resultados

--- src/components/Cotizacion.js
+++ src/components/Cotizacion.js
@@ -7,6 +7,11 @@ const Cotizacion = ({resultado}) => {
     return (
         <div className="resultado">
             <h2>Resultado</h2>
+            <p className="precio">El precio es <span>{resultado.PRICE}</span></p>
+            <p>Precio mas alto del dia: <span>{resultado.HIGHDAY}</span></p>
+            <p>Precio mas bajo del dia: <span>{resultado.LOWDAY}</span></p>
+            <p>Variacion ultimas 24 horas: <span>{resultado.CHANGEPCT24HOUR}%</span></p>
+            <p>Ultima Actualizacion: <span>{resultado.LASTUPDATE}</span></p>
         </div>
     );
 }
